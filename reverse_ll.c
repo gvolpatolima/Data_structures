8dc22f1f7d79ac7020b512a073d79b47f51b9337
Gabriel Volpato <gvolpatolima@gmail.com>
2023-05-14 13:51:24 -0300

    reverse linked list

---
+++ reverse_ll.c
@@ -0,0 +1,61 @@
+#include<stdlib.h>
+#include<stdio.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* Insert(struct Node* head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        struct Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    return head;
+}
+
+void Print(struct Node* head) {
+    struct Node* temp = head;
+    printf("List: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+struct Node* Reverse(struct Node* head) {
+    struct Node* next, *prev, *current;
+    current = head;
+    prev = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
+}
+
+int main() {
+    struct Node* head = NULL;
+    head = Insert(head, 2);
+    head = Insert(head, 4);
+    head = Insert(head, 6);
+    head = Insert(head, 5);
+    Print(head);
+    head = Reverse(head);
+    Print(head);
+
+    return 0;
+}
